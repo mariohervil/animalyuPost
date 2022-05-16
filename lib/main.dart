518ff2014805e4485d035a3a8725fda91cc25bc7
Kevin-sssss <kevinzamjim@campus.monlau.com>
2022-05-16 17:09:50 +0200

    googleMaps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,88 +14,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final Set<Marker> markers = new Set(); //markers for google map
-
-  final LatLng _center = const LatLng(41.38879, 2.15899);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+            onTap: () {
+              print("Clicked: ${office.name}");
+            }
+        );
+        _markers[office.name] = marker;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Animalyu'),
+          title: const Text('Google Office Locations'),
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
-          // onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 13.0,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(0, 0),
+            zoom: 2,
           ),
-          markers: getmarkers(), //markers to show on map
-          mapType: MapType.normal, //map type
-          onMapCreated: (controller) {
-            //method called when map is created
-            setState(() {
-              mapController = controller;
-            });
-          },
+          markers: _markers.values.toSet(),
         ),
       ),
     );
-  }
-
-  Set<Marker> getmarkers() {
-    //markers to place on map
-    setState(() {
-      markers.add(Marker(
-        //add first marker
-        markerId: MarkerId(_center.toString()),
-        position: _center,
-        //position of marker
-        infoWindow: InfoWindow(
-          //popup info
-          title: 'Marker Title First ',
-          snippet: 'My Custom Subtitle',
-        ),
-        onTap: () {
-
-            print("ola");
-
-
-        },
-        icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-      ));
-    });
-    markers.add(Marker(
-      //add second marker
-      markerId: MarkerId(_center.toString()),
-      position: LatLng(42.38879, 1.15899), //position of marker
-      infoWindow: InfoWindow(
-        //popup info
-        title: 'Marker Title Second ',
-        snippet: 'My Custom Subtitle',
-      ),
-      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-    ));
-    markers.add(Marker(
-      //add second marker
-      markerId: MarkerId(_center.toString()),
-      position: LatLng(44.38879, 4.15899), //position of marker
-      infoWindow: InfoWindow(
-        //popup info
-        title: 'Marker Title Second ',
-        snippet: 'My Custom Subtitle',
-      ),
-      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-    ));
-    return markers;
   }
 }
