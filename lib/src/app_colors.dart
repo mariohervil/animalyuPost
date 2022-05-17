0324e40d1f1a3503a37dd1ed57fd842ad628c404
Mario Hernandez <61046979+mariohervil@users.noreply.github.com>
2022-05-17 19:40:54 +0200

    a

---
+++ lib/src/app_colors.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+// Color converter: https://www.w3schools.com/colors/colors_converter.asp// Transparency list
+// 100% FF
+// 95%  F2
+// 90%  E6
+// 87%  DE
+// 85%  D9
+// 80%  CC
+// 75%  BF
+// 70%  B3
+// 65%  A6
+// 60%  99
+// 55%  8C
+// 54%  8A
+// 50%  80
+// 45%  73
+// 40%  66
+// 35%  59
+// 32%  52
+// 30%  4D
+// 26%  42
+// 25%  40
+// 20%  33
+// 16%  29
+// 15%  26
+// 12%  1F
+// 10%  1A
+// 5%   0D
+class AppColors {
+  static const Color darkBackground = Color(0xff100A2C);
+  static const Color text = Color(0xff161616);
+  static const Color background = Color(0xff2B2590);
+  static const Color disabled = Color(0xff989898);
+  static const Color error = Color(0xffff7466);
+  static const Color greyText = Color(0xff565656);
+  static const Color maticToken = Color(0xff85ECE9);
+  static const Color sendPageBackground = Color(0xffBCB2FE);
+  static const Color activeColor = Color(0xff241050);
+  static const Color greyInvoice = Color(0xffc4c4c4);
+}
--- lib/src/profile.dart
+++ lib/src/profile.dart
@@ -1,12 +1,13 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_in_flutter/src/app_colors.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(ProfileDesign());
 }
-
-
 
 class ProfileDesign extends StatefulWidget {
   @override
@@ -20,7 +21,6 @@ class _ProfileDesignState extends State<ProfileDesign> {
       title: 'Profile',
       home: Profile(),
       debugShowCheckedModeBanner: false,
-
     );
   }
 
@@ -47,54 +47,111 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
     return ClipPath(
       clipper: CClipper(),
       child: Container(
-          decoration: const BoxDecoration(color: Colors.redAccent, boxShadow: [
-            BoxShadow(
-              color: Colors.black,
-              blurRadius: 20,
-              offset: Offset(0, 0),
-            ),
-          ]),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.menu),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  AppColors.darkBackground,
+                  AppColors.background,
+                ]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 20,
+                offset: Offset(0, 0),
+              ),
+            ]),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                const Text(
+                  "Profile",
+                  style: TextStyle(
                     color: Colors.white,
-                    onPressed: () {},
+                    fontSize: 16,
                   ),
-                  const Text(
-                    "Profile",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.notifications),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://images3.memedroid.com/images/UPLOADED753/618f10769197d.jpeg"))),
+            ),
+            Text(
+              "Nombre",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 0, 100, 0),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Align(
+                  child: Container(
+                    child: Text(
+                      "data",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    decoration: BoxDecoration(color: Colors.white),
+                  ),
+                  alignment: Alignment.centerLeft,
+                ),
+                Align(
+                    child: Transform.rotate(
+                  angle: (math.pi * 0.055),
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    width: 110,
+                    height: 32,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(16))),
+                    child: Center(
+                      child: Text(
+                        "Edit",
+                        style: TextStyle(color: Colors.black),
+                      ),
                     ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.notifications),
-                    color: Colors.white,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              Container(
-                decoration: BoxDecoration(
-
-                ),
-              )
-            ],
-          )),
+                ))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 
   @override
   // TODO: implement preferredSize
-  Size get preferredSize => const Size(double.infinity, 200);
+  Size get preferredSize => const Size(double.infinity, 450);
 }
-
-
-
 
 class CClipper extends CustomClipper<Path> {
   @override
