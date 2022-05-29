345783ab2849e7d5c53ed11f731a53803cfe0bb1
mario <mariohervil@campus.monlau.com>
2022-05-29 15:21:55 +0200

    Mario 29/05

--- lib/src/animal_scroll_page.dart
+++ lib/src/animal_scroll_page.dart
@@ -1,20 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:ui' as ui;
-import 'package:flutter/gestures.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_maps_in_flutter/src/page_arguments.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_maps_in_flutter/src/app_colors.dart';
-import 'dart:math' as math;
-
 import 'animal.dart';
+
+void main() => runApp(AnimalScroll());
 
 class AnimalScroll extends StatefulWidget {
   const AnimalScroll({Key? key}) : super(key: key);
+
 
   @override
   State<AnimalScroll> createState() => _AnimalScrollState();
@@ -22,57 +17,89 @@ class AnimalScroll extends StatefulWidget {
 
 class _AnimalScrollState extends State<AnimalScroll> {
   final url = "https://animalyu.monlau-smx.com/test/php/phpPruebaProj.php";
-  var list = ["one", "two", "three", "four"];
+  late List<Animals> list;
+  final Map<String, String> body = {
+    'mode': 'selectAllAnimals',
+  };
 
-  void FillAnimals(){
-
-
-  }
-
-
-  Future<List<String>> getAnimals() async {
-    Map<String, String> body = {
-      'mode': 'selectAnimals',
-    };
-
-    http.Response animalData = await makePostRequest(url, body);
-    Map results =  json.decode(animalData.body);
-    Animal animal = Animal.fromJson(results);
-    List<String> animals = animalData.body.split(", ");
-    for(String field in animals){
-
-
-    }
-
-  }
-
-  Future<http.Response> makePostRequest(
-      String url, Map<String, String> body) async {
+  /* Future<Animal> receiveAnimal() async {
     HttpOverrides.global = MyHttpOverrides();
     final response = await http.post(Uri.parse(url), body: body);
-    print(response.body);
-    return response;
-  }
+    final animals = (response.body.replaceAll("][", ","));
+    final List parsedList = json.decode(animals);
+    List<Animal> list = parsedList.map((val) => Animal.fromJson(val)).toList();
+    list.removeAt(0);
 
-  void postData() async {
-    Map<String, String> registerBody = {
-      'mode': 'selectAnimals',
-    };
-    makePostRequest(url, registerBody);
+    return animalFromJson(animals);
+  }*/
+
+  Future<Animals> getAnimals() async {
+    HttpOverrides.global = MyHttpOverrides();
+    final response = await http.post(Uri.parse(url), body: body);
+    final animals = ("${response.body.replaceAll("][", ",")}");
+    //print(animals);
+    final List parsedList = json.decode(animals);
+    List<Animal> list = parsedList.map((val) => Animal.fromJson(val)).toList();
+    Animals animal = Animals(animals: list);
+    return animal;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        for (var item in list)
-          Row(
-            children: [
-              Card(child: Text("- $item\n\n")),
-            ],
-          )
-      ],
+    return MaterialApp(home: FutureBuilder(
+      builder: (context, AsyncSnapshot<Animals> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return _AnimalsList(snapshot.data!.animals);
+        }
+      },
+      future: getAnimals(),
+    )
+    );
+  }
+}
+
+class _AnimalsList extends StatelessWidget {
+  final List<Animal> animals;
+  _AnimalsList(this.animals);
+  @override
+  Widget build(BuildContext context) {
+    animals.removeAt(0);
+    return Container(
+        color: Colors.white,
+        child: ListView.builder(
+          itemCount: animals.length,
+          itemBuilder: ((context, i) {
+            return Card(color: Colors.blueAccent,
+                shape:RoundedRectangleBorder(
+                borderRadius:BorderRadius.circular(24)),
+              clipBehavior:Clip.antiAlias, child:
+            Column(
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Ink.image(
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1143&q=80'
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                      ),
+                      height: 240,
+                      fit: BoxFit.cover,
+                    ),
+
+                    Positioned(bottom:16, right:16, left:16, child: Text(animals[i].name, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24))),
+                  ],
+                ),
+                Text(animals[i].animalType,  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24))
+              ],
+            ),
+            );
+          }),
+        )
     );
   }
 }
@@ -85,3 +112,39 @@ class MyHttpOverrides extends HttpOverrides {
           (X509Certificate cert, String host, int port) => true;
   }
 }
+/*Future<Animal> getData() async {
+  var animals= await makePostRequest(url); // not sure what your events data/method is
+  for(Animal animal in animals){
+  return animal;
+  }
+
+  }
+  FutureBuilder(
+  future: makePostRequest(url),
+  builder: (context, snapshot) {
+  List<Animal> animals = snapshot.data;
+  if (!snapshot.hasData) return CircularProgressIndicator();
+  return new ListView.builder(
+  itemBuilder: (BuildContext context, int index) {  },
+  itemCount:animals.length)
+  }
+*/
+
+/**
+ *
+ * Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    for (Animal animal in list)
+    ListView.builder(
+    itemCount: list.length,
+    itemBuilder: (context, index) {
+    return Card(child:Text(list[index].name));
+    },
+    ),
+    ],
+    );ç
+
+
+ *
+ */
